673817fe2b15e543ab8758f99bea54eec951b0c8
Bernhard Bablok <bablokb@gmx.de>
2024-04-10 15:36:57 +0200

    calculate next update time based on OpenMeteo update-interval

--- src/main.py
+++ src/main.py
@@ -41,6 +41,44 @@ class OpenMeteo(Application):
     super().__init__(data_provider,ui_provider,with_rtc=False,debug=DEBUG)
     self.blink(0.1,color=Application.GREEN)
 
+    self.data["last_update"] = "unknown"
+    self._last_run = 0
+    self._next_update = 0
+
+  # --- calculate interval until next update   -------------------------------
+
+  def _calc_next_update_time(self):
+    """ return interval until next update is due """
+
+    if self.data["last_update"] == "unknown":
+      # very first update - start with 5 minutes
+      self.data["last_update"] = self.data["current"].update
+      interval = 300
+    elif self.data["last_update"] == self.data["current"].update:
+      # no change - keep 5 minutes
+      interval = 300
+    else:
+      # data was updated - use interval of data
+      self.data["last_update"] = self.data["current"].update
+      interval = self.data["current"].interval
+
+    self.msg(f"calculated interval for next update: {interval}")
+    self._next_update = self._last_run + interval
+
+  # --- main code (override base class)   ------------------------------------
+
+  def run(self):
+    """ main code """
+
+    if time.monotonic() < self._next_update:
+      # nothing to do yet
+      return
+    else:
+      # execute standard code and update timings
+      super().run()
+      self._last_run = time.monotonic()
+      self._calc_next_update_time()
+
   # --- key-handler for PyGame-Display environment   -------------------------
 
   def on_event(self,ev):
@@ -48,13 +86,6 @@ class OpenMeteo(Application):
 
     if ev.key in [pygame.K_ESCAPE,pygame.K_q]:
       sys.exit(0)
-
-  # --- event-handler for PyGame-Display environment   -----------------------
-
-  def on_time(self):
-    """ process regular action """
-    # TODO: only run if OpenMeteo update-interval has expired
-    self.run()
 
   # --- main loop for PyGame-Display environment   ---------------------------
 
@@ -64,7 +95,7 @@ class OpenMeteo(Application):
     self.run()
     self.display.event_loop(
       interval=60,
-      on_time=self.on_time, on_event=self.on_event, events=[pygame.KEYDOWN])
+      on_time=self.run, on_event=self.on_event, events=[pygame.KEYDOWN])
 
   # --- main loop for normal CircuitPython environment   ---------------------
 
@@ -72,10 +103,8 @@ class OpenMeteo(Application):
     """ main-loop for normal environment """
 
     while True:
-      start = time.monotonic()
       self.run()
-      # TODO: sleep for OpenMeteo update-interval
-      self.sleep(60 - (time.monotonic()-start))
+      self.sleep(60)        # check once a minute if an update is necessary
 
 # --- main application code   -------------------------------------------------
 
